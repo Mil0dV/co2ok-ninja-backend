e55ef4f83ad1916f5bf08102297d1e45b55c0efe
Cayde7 <43235246+Cayde7@users.noreply.github.com>
2018-10-02 13:32:04 +0200

    Parnters
    New parnters are the best :D

--- Functions.php
+++ Functions.php
@@ -84,6 +84,10 @@
 		$awin = array("jaarbeurs", "gearbest");
 		$anrdoezrs = array("101shavers");
 		$tcfjump = array("0cm");
+		$booking = array("booking");
+		$bol = array("bol");
+		$Cool = array("coolblue");
+		$DX = array("dx");
 
 		if (in_array($Site, $awin)) {
 			header('Location: http://www.awin1.com/cread.php?awinmid=8315&awinaffid=283879&clickref=chex&p=' . $encode);
@@ -91,6 +95,38 @@
 			header('Location: http://www.anrdoezrs.net/links/8106588/type/dlg/sid/chex/' . $encode);
 		} elseif (in_array($Site, $tcfjump)) {
 			header('Location:https://t.cfjump.com/50770/t/48059?Url='. $encode . '&UniqueId=chex');
+		}elseif (in_array($Site, $Al)) {
+			header('Location:http://www.aluminiumwarehouse.co.uk/tin/?tt=9296_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $Arena)) {
+			header('Location:http://www.arenascollection.com/design/?tt=1673_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $BangGoods)) {
+			header('Location:http://www.banggood.com/bang/?tt=16943_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $Basic)) {
+			header('Location:http://www.basic-travel.com/travel/?tt=2200_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $BirdFOOT)) {
+			header('Location:http://www.birdfood.co.uk/tt/?tt=6544_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $biv)) {
+			header('Location:http://www.bivolino.com/custom/index.aspx?tt=12298_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $Blind)) {
+			header('Location:http://www.blindsdirectonline.co.uk/blinds/?tt=24293_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $Buy)) {
+			header('Location:http://www.buyincoins.com/shopping/?tt=25773_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $Camping)) {
+			header('Location:http://www.campingselection.com/uk/Partners/TradeTracker/?tt=8294_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $ClickBaid)) {
+			header('Location:https://click.linksynergy.com/deeplink?id=XK9XruzkyUo&u1=chex&mid=35490&murl='. $encode);
+		}elseif (in_array($Site, $Colway)) {
+			header('Location:https://colwayinternational.online/beauty/?tt=26078_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $A4)) {
+			header('Location:https://www.a4labels.com/labels/?tt=25370_12_282840_chex&r='. $encode);
+		}elseif (in_array($Site, $CT)) {
+			header('Location:https://www.chicretreats.com/hotels/?tt=25704_12_282840_chex&r='. $encode);
+		}elseif(in_array($Site, $bol)) {
+			header('Location:https://partner.bol.com/click/click?p=2&t=url&s=51851&f=TXL&url=' . $encode);
+		}elseif(in_array($Site, $Cool)) {
+			header('Location:https://partner.bol.com/click/click?p=2&t=url&s=51851&f=TXL&url=' . $encode);
+		}elseif(in_array($Site, $DX)) {
+			header('Location:' . $url . '?TC=USD&&Utm_rid=78139600&Utm_source=affiliate');
 		} else{
 			header('Location: http://rover.ebay.com/rover/1/' . $Als . '/1?ff3=4&pub=5575349754&toolid=11800&campid=5338219191&customid=chex&mpre=' . $encode);
 		}
